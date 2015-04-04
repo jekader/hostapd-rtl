a6073b18fc6a7e22b6126b9747d4c3abd7847cfa
jekader <jekader@gmail.com>
2015-04-04 22:04:21 +0200

    fix typo

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-echo "please ensure you install build-essential fir"
+echo "Please ensure you install the toolchain and build dependencies first!"
 make clean && cd .. && tar -cvzf hostapd-rtl_2.4.orig.tar.gz hostapd-rtl && cd hostapd-rtl
 debuild -us -uc
